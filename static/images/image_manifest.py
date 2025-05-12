f09dab6570cc1ab869c29f9d195633d0518f52f5
Barklee sanders <barkleesanders@Barklees-MacBook-Pro.local>
2025-05-11 17:46:59 -0700

    Remove color variant images from static/images as per standards cleanup

---
+++ static/images/image_manifest.py
@@ -0,0 +1,10 @@
+# Maps canonical car names to image filenames
+CAR_IMAGE_MAP = {
+    "bmw_m4": "BMW_M4.png",
+    "ferrari_laferrari": "FERRARI_LAFERRARI.png",
+    "ford_model_t": "FORD_MODEL_T.png",
+    "ford_mustang": "FORD_MUSTANG..png",
+    "jaguar_e_type": "JAGUAR_E-TYPE.png",
+    "mclaren_artura": "MCLAREN_ARTURA.png",
+    "porsche_911": "PORSCHE_911.png",
+}
